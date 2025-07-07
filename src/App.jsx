830bd20b356fad4cf40113d29e462f1e5bc1dc92
gxuoo <rbdud189@naver.com>
2025-07-07 16:17:38 +0900

    style: 초기 세팅 (prettier 적용)
    세미 콜론, 큰 따옴표 등 코드 포멧팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>그리디 미션 : 뉴스 웹 뷰어 시이작!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
