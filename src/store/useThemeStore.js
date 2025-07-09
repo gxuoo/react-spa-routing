7af195fffce6ed39762a9ade9abe864d6d0738e6
gxuoo <rbdud189@naver.com>
2025-07-09 16:46:16 +0900

    feat: zustand를 사용한 테마 전역 상태 추가
    tailwind에서 dark 모드 활성화 설정 추가

---
+++ src/store/useThemeStore.js
@@ -0,0 +1,14 @@
+// src/store/useThemeStore.js
+import { create } from "zustand";
+
+const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("theme") || "light",
+  toggleTheme: () =>
+    set((state) => {
+      const newTheme = state.theme === "light" ? "dark" : "light";
+      localStorage.setItem("theme", newTheme);
+      return { theme: newTheme };
+    }),
+}));
+
+export default useThemeStore;
