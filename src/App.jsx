a6bde07a49d78c24d3b5b6bb19f4ef663d87f4ff
gxuoo <rbdud189@naver.com>
2025-07-09 16:46:44 +0900

    feat: Dark Mode 추가 및 Light Mode Toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import useThemeStore from "./store/useThemeStore";
 import Router from "./router/Router";
 
 const App = () => {
+  const theme = useThemeStore((state) => state.theme);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (theme === "dark") {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [theme]);
+
   return <Router />;
 };
 
